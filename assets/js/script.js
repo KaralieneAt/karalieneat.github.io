ebd6ac33dca5830ee95cb32812a1b252cf452c0b
Romina <romina.collio@gmail.com>
2022-06-29 21:56:23 -0400

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+    $("#enviarmensaje").click(function () {
+        alert("El mensaje se envio correctamente");
+    })
+
+    
+    $(".title").click(function () {
+        $(this).css({
+            "color": "#31C82A"
+        })
+    })
+
+    $(".text").click(function () {
+        $(this).css({
+            "color": "#A558E1"
+        })
+    })
+
+    $(".textimage").click(function () {
+        $(this).css({
+            "color": "#3E2365"
+        })
+    })
+
+    $(".card-header").click(function () {
+        $(".text").toggle();
+    });
+
+
+
+    $("#one").click(function () {
+        $("#bodyone").toggle();
+    });
+
+    $("#two").click(function () {
+        $("#bodytwo").toggle();
+    });
+
+    $("#three").click(function () {
+        $("#bodythree").toggle();
+    });
+
+    $("#for").click(function () {
+        $("#bodyfor").toggle();
+    });
+
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+})
